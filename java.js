660278732f6b047f8f399cf5954ec81ff9a7acef
Arca91 <131290095+Arca91@users.noreply.github.com>
2023-05-16 19:44:30 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,22 @@
+// let numero= parseInt(prompt("inserisci il numero"))
+// function num(numero){
+//     if(numero >0) {
+//         console.log("e un numero pari")
+//     } else if (numero <0) {
+//         console.log("e un valore disparo")
+//     }
+   
+// }
+// console.log(num(numero));
+// let primo=parseInt(prompt(`inserisci il primo numero`));
+// let secondo=parseInt(prompt(`iniserisci il secondo numero`));
+// function controllo(primo,secondo){
+//     if(primo === secondo) {
+//         console.log(`true`)
+//     } else{
+//         console.log(`Falso`)
+//     }
+    
+// }
+// console.log(controllo(primo, secondo))
+
