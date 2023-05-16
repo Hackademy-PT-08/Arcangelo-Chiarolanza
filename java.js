ef955f25fde9e576420184ecfde41bac41fc3ac7
Arca91 <131290095+Arca91@users.noreply.github.com>
2023-05-16 21:30:57 +0200

    Update java.js

--- java.js
+++ java.js
@@ -1,22 +1,73 @@
-// let numero= parseInt(prompt("inserisci il numero"))
-// function num(numero){
-//     if(numero >0) {
-//         console.log("e un numero pari")
-//     } else if (numero <0) {
-//         console.log("e un valore disparo")
-//     }
+// // let numero= parseInt(prompt("inserisci il numero"))
+// // function num(numero){
+// //     if(numero >0) {
+// //         console.log("e un numero pari")
+// //     } else if (numero <0) {
+// //         console.log("e un valore disparo")
+// //     }
    
-// }
-// console.log(num(numero));
-// let primo=parseInt(prompt(`inserisci il primo numero`));
-// let secondo=parseInt(prompt(`iniserisci il secondo numero`));
-// function controllo(primo,secondo){
-//     if(primo === secondo) {
-//         console.log(`true`)
-//     } else{
-//         console.log(`Falso`)
-//     }
+// // }
+// // console.log(num(numero));
+// // let primo=parseInt(prompt(`inserisci il primo numero`));
+// // let secondo=parseInt(prompt(`iniserisci il secondo numero`));
+// // function controllo(primo,secondo){
+// //     if(primo === secondo) {
+// //         console.log(`true`)
+// //     } else{
+// //         console.log(`Falso`)
+// //     }
     
+// // }
+// // console.log(controllo(primo, secondo))
+
+// let frase= prompt(` scrivi una frase`)
+
+// let vocale_a = 0;
+// let vocale_b = 0;
+// let vocale_c=  0;
+// let vocale_d = 0;
+// let vocale_e = 0;
+// let totale_let= 0;
+
+// for (let i = 0; i < frase.length; i++) {
+//       let char = frase.charAt(i).toLowerCase();
+// //   if(frase[i]===vocale_a){
+// //         console.log(frase[i])
+    
+//  switch(char){
+
+
+//  case "a":
+//     vocale_a +=1;
+//      totale_let+= 1;
+//     break;   
+
+
+// case "b":
+//     vocale_b +=1;
+//     totale_let += 1;
+//     break;   
+
+//     case "c":
+//     vocale_c +=1;
+//     totale_let += 1;
+//     break;  
+
+//     case "d":
+//         vocale_d +=1;
+//         totale_let += 1;
+//         break;  
+
+//         case "e":
+//             vocale_e +=1;
+//              totale_let+= 1;
+//             break;  
+            
+           
+
 // }
-// console.log(controllo(primo, secondo))
+
+// }
+// console.log("Totale", totale_let ," totale lettere A ", vocale_a, "totale lettere B", vocale_b, "toale lettere C", 
+// vocale_c, "totale lettere D", vocale_d,"totale lettere E" ,vocale_e);
 
