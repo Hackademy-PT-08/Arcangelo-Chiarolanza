654f21f1bcae99ba99d4cd97618e99450023ce1c
Arca91 <131290095+Arca91@users.noreply.github.com>
2023-05-16 21:39:27 +0200

    Update java.js

--- java.js
+++ java.js
@@ -1,38 +1,38 @@
-// // let numero= parseInt(prompt("inserisci il numero"))
-// // function num(numero){
-// //     if(numero >0) {
-// //         console.log("e un numero pari")
-// //     } else if (numero <0) {
-// //         console.log("e un valore disparo")
-// //     }
+ //  let numero= parseInt(prompt("inserisci il numero"))
+//  function num(numero){
+//      if(numero >0) {
+//        console.log("e un numero pari")
+//     } else if (numero <0) {
+//        console.log("e un valore disparo")
+//     }
    
-// // }
-// // console.log(num(numero));
-// // let primo=parseInt(prompt(`inserisci il primo numero`));
-// // let secondo=parseInt(prompt(`iniserisci il secondo numero`));
-// // function controllo(primo,secondo){
-// //     if(primo === secondo) {
-// //         console.log(`true`)
-// //     } else{
-// //         console.log(`Falso`)
-// //     }
+//  }
+//  console.log(num(numero));
+//let primo=parseInt(prompt(`inserisci il primo numero`));
+//  let secondo=parseInt(prompt(`iniserisci il secondo numero`));
+//  function controllo(primo,secondo){
+//      if(primo === secondo) {
+//          console.log(`true`)
+//    } else{
+ // //         console.log(`Falso`)
+//     }
     
-// // }
-// // console.log(controllo(primo, secondo))
+//  }
+//  console.log(controllo(primo, secondo))
 
 // let frase= prompt(` scrivi una frase`)
 
 // let vocale_a = 0;
-// let vocale_b = 0;
-// let vocale_c=  0;
-// let vocale_d = 0;
 // let vocale_e = 0;
+// let vocale_i=  0;
+// let vocale_o = 0;
+// let vocale_u = 0;
 // let totale_let= 0;
 
 // for (let i = 0; i < frase.length; i++) {
 //       let char = frase.charAt(i).toLowerCase();
-// //   if(frase[i]===vocale_a){
-// //         console.log(frase[i])
+//    if(frase[i]===vocale_a){
+//         console.log(frase[i])
     
 //  switch(char){
 
@@ -43,23 +43,23 @@
 //     break;   
 
 
-// case "b":
-//     vocale_b +=1;
+// case "e":
+//     vocale_e +=1;
 //     totale_let += 1;
 //     break;   
 
-//     case "c":
-//     vocale_c +=1;
+//     case "i":
+//     vocale_i +=1;
 //     totale_let += 1;
 //     break;  
 
-//     case "d":
-//         vocale_d +=1;
+//     case "o":
+//         vocale_o +=1;
 //         totale_let += 1;
 //         break;  
 
-//         case "e":
-//             vocale_e +=1;
+//         case "u":
+//             vocale_u +=1;
 //              totale_let+= 1;
 //             break;  
             
@@ -68,6 +68,7 @@
 // }
 
 // }
-// console.log("Totale", totale_let ," totale lettere A ", vocale_a, "totale lettere B", vocale_b, "toale lettere C", 
-// vocale_c, "totale lettere D", vocale_d,"totale lettere E" ,vocale_e);
+// console.log("Totale", totale_let ," totale lettere A ", vocale_a, "totale lettere e", vocale_e, "toale lettere i", 
+// vocale_i, "totale lettere o", vocale_o,"totale lettere u" ,vocale_u);
+
 
